071cbe290a5e8f4922f9ebbc1924381d123bef13
paulnyabaro <paulnyabaro1@gmail.com>
2025-02-06 09:51:04 +0300

    Working on nav and header section

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
 
 <footer>
-
+    <div class="container">
+        <div class="grid grid-row-4">
+            <div class="grid-1">
+                <h3>About</h3>
+                <p>Deserunt inventore eveniet, quisquam illo dolorum earum ex magni, hic obcaecati repellat deleniti culpa.</p>
+            </div>
+        </div>
+    </div>
 </footer>
 <script src="js/script.js"></script>
 </body>
--- index.php
+++ index.php
@@ -4,5 +4,11 @@
             <h1>The main area, here you'll learn JS, PHP and CSS</h1>
         </div>
     </header>
+    <section>
+        <div class="container">
+            <h2>What's covered here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquid maiores, autem non delectus impedit natus accusantium sint? Expedita id sapiente autem fuga a aspernatur, ab enim, vitae porro similique modi dolor cumque.</p>
+        </div>
+    </section>
 <?php include 'footer.php'; ?>
     
